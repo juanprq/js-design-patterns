0c59a6dfee5aa4a048a5faa6ed66435b5f9694ff
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-03-02 20:57:52 -0500

    Adds dependency injection pattern

---
+++ src/modules/dependencyInjection/bank.js
@@ -0,0 +1,67 @@
+const accounts = [];
+
+const findAccount = Symbol('findAccount');
+const trimName = (name) => `XXXXXXXX${name .replace(/\s/g, '')
+  .slice(-4)
+  .toUpperCase()}`;
+
+class Bank {
+  config({ logger }) {
+    this.logger = logger;
+  }
+
+  [findAccount](name) {
+    return accounts.find(acc => acc.name === name);
+  }
+
+  createAccount(name, monies = 0) {
+    if (!this[findAccount](name)) {
+      accounts.push({
+        name,
+        monies,
+      });
+
+      if (this.logger) {
+        this.logger.write(`Account created for ${trimName(name)}`);
+      }
+    } else {
+      console.log('An account already exists');
+    }
+  }
+
+  credit(name, amount) {
+    if (this[findAccount](name)) {
+      accounts.forEach((acc) => {
+        if (acc.name === name) {
+          acc.monies += amount;
+        }
+      });
+
+      if (this.logger) {
+        this.logger.write(`${amount} credited to ${trimName(name)}`);
+      }
+    }
+  }
+
+  debit(name, amount) {
+    if (this[findAccount](name)) {
+      accounts.forEach((acc) => {
+        acc.monies -= amount;
+      });
+    }
+
+    if (this.logger) {
+      this.logger.write(`${amount} debited from ${trimName(name)}`);
+    }
+  }
+
+  getFunds(name) {
+    if (this[findAccount](name)) {
+      return accounts.find((acc) => acc.name === name).monies;
+    }
+
+    return 'Account not found!';
+  }
+}
+
+module.exports = new Bank();
---
+++ src/modules/dependencyInjection/consoleLogger.js
@@ -0,0 +1,4 @@
+const write = (log) => console.log(`${new Date()} : ${log}`);
+
+module.exports = { write };
+
